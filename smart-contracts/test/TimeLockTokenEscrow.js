c8d33c7f1b74f820ecef81753d6464c4fbdee78f
James Morgan <james.morgan.e@googlemail.com>
2020-05-18 15:57:40 +0100

    Multiple deposits for the same address

--- smart-contracts/test/TimeLockTokenEscrow.js
+++ smart-contracts/test/TimeLockTokenEscrow.js
@@ -7,147 +7,121 @@ require('chai').should();
 const TimeLockTokenEscrow = contract.fromArtifact('TimeLockTokenEscrow');
 const TestToken = contract.fromArtifact('TestToken');
 
-function now() {
-    return Math.floor(Date.now() / 1000);
-}
-
-describe('TimeLockTokenEscrow V2 tests', function () {
+describe('TimeLockTokenEscrow V2 tests', async () => {
 
     const [creator, beneficiary1, random] = accounts;
 
-    beforeEach(async function () {
+    beforeEach(async () => {
         this.initialSupply = new BN('1000000').mul(new BN('10').pow(new BN('18')));
         this.token = await TestToken.new({from: creator});
         this.timeLockTokenEscrow = await TimeLockTokenEscrow.new(this.token.address, {from: creator});
     });
 
-    const lockupTokens = async function (token, escrowContract, beneficiary, amount, lockedUntil) {
+    const lockupTokens = async (beneficiary, amount, lockedUntil) => {
         // Approve the contract first and check its allowance after
-        await token.approve(escrowContract.address, amount, {from: creator});
+        await this.token.approve(this.timeLockTokenEscrow.address, amount, {from: creator});
 
-        (await token.allowance(creator, escrowContract.address)).should.be.bignumber.equal(amount);
+        (await this.token.allowance(creator, this.timeLockTokenEscrow.address)).should.be.bignumber.equal(amount);
 
         // Lock up the tokens
-        return await escrowContract.lock(beneficiary, amount, lockedUntil, {from: creator});
+        return await this.timeLockTokenEscrow.lock(beneficiary, amount, lockedUntil, {from: creator});
+    };
+
+    const validDeposit = async (id, beneficiary1, {creator, amount, lockedUntil}) => {
+        const {_creator, _amount, _lockedUntil} = await this.timeLockTokenEscrow.getLockForDepositIdAndBeneficiary(id, beneficiary1);
+        _creator.should.be.equal(creator);
+        _amount.should.be.bignumber.equal(amount);
+        _lockedUntil.should.be.bignumber.equal(lockedUntil);
     };
 
     const amountToLockUp = new BN('5000');
+    const secondAmountToLockUp = new BN('2500');
 
-    describe('lock() - Locking up tokens', function () {
+    describe('lock() - Locking up tokens', async () => {
 
-        describe('happy path', function () {
+        describe('happy path', async () => {
 
-            const oneHourFromNow = new BN((now() + (60 * 60)).toString());
+            beforeEach(async () => {
 
-            beforeEach(async function () {
+                this.oneHourFromNow = new BN(((await time.latest()) + (60 * 60)).toString());
+
                 // Ensure timeLockTokenEscrow has no tokens
                 (await this.token.balanceOf(this.timeLockTokenEscrow.address)).should.be.bignumber.equal('0');
 
-                const {logs} = await lockupTokens(
-                    this.token,
-                    this.timeLockTokenEscrow,
+                this.receipt = await lockupTokens(
                     beneficiary1,
                     amountToLockUp,
-                    oneHourFromNow
+                    this.oneHourFromNow
                 );
-                this.logs = logs;
             });
 
-            it('Locks up a specified amount of tokens', async function () {
-                await expectEvent.inLogs(this.logs, 'Lockup', {
+            it('Locks up a specified amount of tokens', async () => {
+                await expectEvent(this.receipt, 'Lockup', {
                     _depositId: '1',
                     _creator: creator,
                     _beneficiary: beneficiary1,
                     _amount: amountToLockUp,
-                    _lockedUntil: oneHourFromNow
+                    _lockedUntil: this.oneHourFromNow
                 });
 
                 // Ensure timeLockTokenEscrow has tokens
                 (await this.token.balanceOf(this.timeLockTokenEscrow.address)).should.be.bignumber.equal('5000');
             });
 
-            it('Reverse mapping setup for new deposit and beneficiary', async function () {
+            it('Reverse mapping setup for new deposit and beneficiary', async () => {
                 const depositIds = await this.timeLockTokenEscrow.getDepositIdsForBeneficiary(beneficiary1);
                 depositIds.should.be.lengthOf(1);
                 depositIds[0].should.be.bignumber.equal('1');
             });
 
-            it('TimeLock data correctly set', async function () {
-                const {
-                    _creator, _amount, _lockedUntil
-                } = await this.timeLockTokenEscrow.getLockForDepositIdAndBeneficiary('1', beneficiary1);
-                _creator.should.be.equal(_creator);
-                _amount.should.be.bignumber.equal(amountToLockUp);
-                _lockedUntil.should.be.bignumber.equal(oneHourFromNow);
+            it('TimeLock data correctly set', async () => {
+                await validDeposit('1', beneficiary1, {
+                    creator: creator,
+                    amount: amountToLockUp,
+                    lockedUntil: this.oneHourFromNow,
+                });
             });
 
         });
 
-        describe('requires', function () {
+        describe('requires', async () => {
 
-            it('Fails to lockup tokens for address zero', async function () {
+            it('Fails to lockup tokens for address zero', async () => {
                 await expectRevert(
                     this.timeLockTokenEscrow.lock(ZERO_ADDRESS, amountToLockUp, '0', {from: creator}),
                     'You cannot lock up tokens for the zero address'
                 );
             });
 
-            it('Fails when amount is zero', async function () {
+            it('Fails when amount is zero', async () => {
                 await expectRevert(
                     this.timeLockTokenEscrow.lock(beneficiary1, '0', '0', {from: creator}),
                     'Lock up amount of zero tokens is invalid'
                 );
             });
-
-            // FIXME
-            it.skip('Fails when tokens have already been locked up for a beneficiary', async function () {
-                // Lock up the tokens
-                const oneHourFromNow = new BN((now() + (60 * 60)).toString());
-                await lockupTokens(
-                    this.token,
-                    this.timeLockTokenEscrow,
-                    beneficiary1,
-                    amountToLockUp,
-                    oneHourFromNow
-                );
-
-                // Try to lock up again
-                await expectRevert(
-                    this.timeLockTokenEscrow.lock(beneficiary1, amountToLockUp, '0', {from: creator}),
-                    'Tokens have already been locked up for the given address'
-                );
-            });
-
-            it('Fails when the contract is not approved to escrow tokens', async function () {
-                await expectRevert(
-                    this.timeLockTokenEscrow.lock(beneficiary1, amountToLockUp, '0', {from: creator}),
-                    'The contract does not have enough of an allowance to escrow'
-                );
-            });
         });
     });
 
-    describe('withdrawal() - Releasing the tokens', function () {
-        describe('happy path', function () {
-            it('Sends the token after the lockup period has ended', async function () {
+    describe('withdrawal() - Releasing the tokens', async () => {
+        describe('happy path', async () => {
+            it('Sends the token after the lockup period has ended', async () => {
                 // Lock up the tokens
-                const oneHourFromNow = new BN((now() + (60 * 60)).toString());
+                this.oneHourFromNow = new BN(((await time.latest()) + (60 * 60)).toString());
+
                 await lockupTokens(
-                    this.token,
-                    this.timeLockTokenEscrow,
                     beneficiary1,
                     amountToLockUp,
-                    oneHourFromNow
+                    this.oneHourFromNow
                 );
 
                 // Ensure beneficiary1 has no tokens
                 (await this.token.balanceOf(beneficiary1)).should.be.bignumber.equal('0');
 
                 // Fast forward time by over an hour
-                await time.increaseTo(oneHourFromNow.add(new BN('3600')));
+                await time.increaseTo(this.oneHourFromNow.add(new BN('3600')));
 
-                const {logs} = await this.timeLockTokenEscrow.withdrawal(1, beneficiary1, {from: random});
-                await expectEvent.inLogs(logs, 'Withdrawal', {
+                const receipt = await this.timeLockTokenEscrow.withdrawal(1, beneficiary1, {from: random});
+                await expectEvent(receipt, 'Withdrawal', {
                     _depositId: '1',
                     _beneficiary: beneficiary1,
                     _caller: random,
@@ -158,10 +132,8 @@ describe('TimeLockTokenEscrow V2 tests', function () {
                 (await this.token.balanceOf(beneficiary1)).should.be.bignumber.equal(amountToLockUp);
             });
 
-            it('Can re-lock up tokens after a withdrawal', async function () {
+            it('Can re-lock up tokens after a withdrawal', async () => {
                 await lockupTokens(
-                    this.token,
-                    this.timeLockTokenEscrow,
                     beneficiary1,
                     amountToLockUp,
                     new BN('0')
@@ -170,8 +142,6 @@ describe('TimeLockTokenEscrow V2 tests', function () {
                 await this.timeLockTokenEscrow.withdrawal(1, beneficiary1, {from: random});
 
                 await lockupTokens(
-                    this.token,
-                    this.timeLockTokenEscrow,
                     beneficiary1,
                     amountToLockUp,
                     new BN('0')
@@ -181,18 +151,16 @@ describe('TimeLockTokenEscrow V2 tests', function () {
             });
         });
 
-        describe('requires', function () {
-            it('Fails when no tokens are locked up for an address', async function () {
+        describe('requires', async () => {
+            it('Fails when no tokens are locked up for an address', async () => {
                 await expectRevert(
                     this.timeLockTokenEscrow.withdrawal(1, random, {from: random}),
                     'There are no tokens locked up for this address'
                 );
             });
 
-            it('Fails when tokens have already been claimed', async function () {
+            it('Fails when tokens have already been claimed', async () => {
                 await lockupTokens(
-                    this.token,
-                    this.timeLockTokenEscrow,
                     beneficiary1,
                     amountToLockUp,
                     new BN('0')
@@ -206,11 +174,9 @@ describe('TimeLockTokenEscrow V2 tests', function () {
                 );
             });
 
-            it('Fails when tokens are still within their lockup period', async function () {
-                const fiveHoursFromNow = new BN((now() + ((60 * 60) * 5)).toString());
+            it('Fails when tokens are still within their lockup period', async () => {
+                const fiveHoursFromNow = new BN(((await time.latest()) + ((60 * 60) * 5)).toString());
                 await lockupTokens(
-                    this.token,
-                    this.timeLockTokenEscrow,
                     beneficiary1,
                     amountToLockUp,
                     fiveHoursFromNow
@@ -224,8 +190,120 @@ describe('TimeLockTokenEscrow V2 tests', function () {
         });
     });
 
-    // FIXME test for multiple deposits
-    // FIXME test for multiple withdrawals
-    // FIXME test for lockup time cross overs
+    describe('allows for multiple deposits and withdrawals for a single address', async () => {
+
+        beforeEach(async () => {
+            this.now = await time.latest();
+            this.oneHourFromNow = new BN(this.now.add(new BN('3600')));
+            this.twoHourFromNow = new BN(this.now.add(new BN('7200')));
+
+            // Ensure timeLockTokenEscrow has no tokens
+            (await this.token.balanceOf(this.timeLockTokenEscrow.address)).should.be.bignumber.equal('0');
+
+            // first lockup
+            await lockupTokens(
+                beneficiary1,
+                amountToLockUp,
+                this.oneHourFromNow
+            );
+
+            // second lockup
+            await lockupTokens(
+                beneficiary1,
+                secondAmountToLockUp,
+                this.twoHourFromNow
+            );
+
+            // Ensure timeLockTokenEscrow now has both tokens
+            (await this.token.balanceOf(this.timeLockTokenEscrow.address)).should.be.bignumber.equal(
+                new BN(amountToLockUp).add(new BN(secondAmountToLockUp))
+            );
+        });
+
+        it('can enumerate deposits for an address', async () => {
+            const depositIds = await this.timeLockTokenEscrow.getDepositIdsForBeneficiary(beneficiary1);
+            depositIds.map(e => e.toString()).should.be.deep.equal(['1', '2']);
+
+            await validDeposit(depositIds[0], beneficiary1, {
+                creator: creator,
+                amount: amountToLockUp,
+                lockedUntil: this.oneHourFromNow
+            });
+
+            await validDeposit(depositIds[1], beneficiary1, {
+                creator: creator,
+                amount: secondAmountToLockUp,
+                lockedUntil: this.twoHourFromNow
+            });
+        });
+
+        it('withdrawing the first deposit', async () => {
+            // Fast forward to post the time lock
+            await time.increaseTo(this.oneHourFromNow);
+
+            const receipt = await this.timeLockTokenEscrow.withdrawal('1', beneficiary1, {from: random});
+            await expectEvent(receipt, 'Withdrawal', {
+                _depositId: '1',
+                _beneficiary: beneficiary1,
+                _caller: random,
+                _amount: amountToLockUp
+            });
+
+            // Check the balance of beneficiary1
+            (await this.token.balanceOf(beneficiary1)).should.be.bignumber.equal(amountToLockUp);
+
+            // Unable to withdraw from first again
+            await expectRevert(
+                this.timeLockTokenEscrow.withdrawal('1', beneficiary1),
+                'There are no tokens locked up for this address'
+            );
+
+            // Unable to withdraw from second yet
+            await expectRevert(
+                this.timeLockTokenEscrow.withdrawal('2', beneficiary1),
+                'Tokens are still locked up'
+            );
+        });
+
+        it('withdrawing the second deposit', async () => {
+            // Fast forward to post the time lock
+            await time.increaseTo(this.twoHourFromNow);
+
+            // Can withdraw from first
+            const receipt = await this.timeLockTokenEscrow.withdrawal('1', beneficiary1, {from: random});
+            await expectEvent(receipt, 'Withdrawal', {
+                _depositId: '1',
+                _beneficiary: beneficiary1,
+                _caller: random,
+                _amount: amountToLockUp
+            });
+
+            // Can withdraw from second
+            const secondWithdrawalReceipt = await this.timeLockTokenEscrow.withdrawal('2', beneficiary1, {from: random});
+            await expectEvent(secondWithdrawalReceipt, 'Withdrawal', {
+                _depositId: '2',
+                _beneficiary: beneficiary1,
+                _caller: random,
+                _amount: secondAmountToLockUp
+            });
+
+            // Check the balance of beneficiary1
+            (await this.token.balanceOf(beneficiary1)).should.be.bignumber.equal(
+                new BN(amountToLockUp).add(new BN(secondAmountToLockUp))
+            );
+
+            // Unable to withdraw from first again
+            await expectRevert(
+                this.timeLockTokenEscrow.withdrawal('1', beneficiary1),
+                'There are no tokens locked up for this address'
+            );
+
+            // Unable to withdraw from second again
+            await expectRevert(
+                this.timeLockTokenEscrow.withdrawal('2', beneficiary1),
+                'There are no tokens locked up for this address'
+            );
+        });
+    });
 
 });
